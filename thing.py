40b92beb79777fc380c735d9c5d1e0c330bebb1b
Andrew Kennedy <amk319@scarletmail.rutgers.edu>
2016-06-07 14:12:07 -0400

    Initial Commit

---
+++ thing.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main (n=901):
+    x= np.random.uniform(low=-1,high=1,size=(n,));
+    x.sort()
+
+    y=x**3 + x**2+ 0.05*np.random.uniform(low=-1,high=1,size=(n,)) ;
+    unused_figure,axes=plt.subplots(nrows=1, ncols=2)
+    axis=axes[0]
+    axis.scatter(x,y)
+    axis=axes[1]
+    axis.plot(x,y)
+    
+#     figure=plt.figure();
+#     axis=figure.add_subplot(1 ,1, 1);
+    
+if __name__=="__main__":
+    main()
+    plt.show()
+    
+    
